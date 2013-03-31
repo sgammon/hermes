c9b40a653a766ce141fd65390a41a1320db1f88c
Sam Gammon <sam.gammon@ampush.com>
2013-03-31 14:16:30 -0700

    Corrected URL in API routing to match Nginx proxied endpoint.

--- app/api/routing.py
+++ app/api/routing.py
@@ -22,7 +22,7 @@ def get_rules():
 	return [
 
 		routes.HandlerPrefixRoute('api.handlers.', [
-			Route('/_api/rpc/test', name='landing', handler='test.TestHandler')
+			Route('/v1/rpc/test', name='landing', handler='test.TestHandler')
 		])
 
 	]
