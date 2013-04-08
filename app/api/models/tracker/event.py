0208d80167069fd34a785f1b923ca4bff1ccaaa4
Sam Gammon <sam.gammon@ampush.com>
2013-04-07 18:50:32 -0700

    Starting Tracker datamodel. First up: `TrackedEvent`.

---
+++ app/api/models/tracker/event.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+Models: Tracker
+
+Contains models used in the `EventTracker` subsystem.
+
+-sam (<sam.gammon@ampush.com>)
+
+'''
+
+# apptools models
+from apptools import model
+
+
+## TrackedEvent - represents a single `EventTracker` URL hit.
+class TrackedEvent(model.Model):
+
+	''' A hit to the `EventTracker` server. '''
+
+	id = basestring, {'required': True}
+	type = basestring
+	profile = basestring, {'default': None}
+	params = dict, {'required': True}
+	timestamp = int, {'required': True}
