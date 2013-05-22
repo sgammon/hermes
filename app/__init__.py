548b1236bf94085bad2bc6d447f2854a710919c4
Sam Gammon <sam.gammon@ampush.com>
2013-05-22 12:00:44 -0700

    Update to apprunner and main.py.

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
-## Package init for `Hermes`.
+
+## Package init for `Hermes.app`.
+import bootstrap
+from main import devserver
+from main import APIServer
+
+if __name__ == '__main__':
+	devserver()
+
