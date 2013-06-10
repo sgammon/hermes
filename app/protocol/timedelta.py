a4727f4f1a76d87ffd373cbadfba3365dc393139
Sam Gammon <sam.gammon@ampush.com>
2013-06-09 18:58:09 -0700

    Added `special` module to protocol to enumerate sentinel/special-value strings, cleaned up `timedelta` protocol for use in aggregation specs, added initial structure and code to calculate/build `Aggregation` specification objects and convert them into index specifications.

--- app/protocol/timedelta.py
+++ app/protocol/timedelta.py
@@ -1,8 +1,34 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+Timedelta Protocol
+
+Description coming soon.
+
+-sam (<sam.gammon@ampush.com>)
+
+'''
+
+# Protocol
+from protocol import meta
 
 
-class TimeWindow():
+# Globals
+_GLOBAL_WINDOW_POSTFIX = '__global__'
 
+
+## TimeWindow
+# Specifies supported aggregation/attribution time windows.
+class TimeWindow(meta.ProtocolDefinition):
+
+    ''' Enumerates available timewindows for aggregation
+        and attribution of properties/values. '''
+
+    # Day Window
     ONE_DAY = 0x1
+
+    # Week-level Windows
     ONE_WEEK = 0x2
     TWO_WEEKS = 0x3
     THREE_WEEKS = 0x4
@@ -10,4 +36,14 @@ class TimeWindow():
     FIVE_WEEKS = 0x6
     SIX_WEEKS = 0x7
 
+    # Month-level Windows
+    MONTH = 0x8
+    TWO_MONTHS = 0x9
+    THREE_MONTHS = 0xA
+    FOUR_MONTHS = 0xB
+    FIVE_MONTHS = 0xC
+    SIX_MONTHS = 0xD
+
+    # Special Windows
+    YEAR = 0x9
     FOREVER = 0x0
