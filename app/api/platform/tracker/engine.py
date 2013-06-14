03dd5310b9afed1f8cd1d589abcb4b686b90da5b
Sam Gammon <sam.gammon@ampush.com>
2013-06-13 20:33:23 -0700

    Cleaned up `EventEngine` (AIRU-1065), updated docstrings, stubbed-out `subscribe` and `index_add` methods.

--- app/api/platform/tracker/engine.py
+++ app/api/platform/tracker/engine.py
@@ -21,7 +21,6 @@ from apptools.util import datastructures
 
 # Model Adapters
 from apptools.model.adapter import redis
-from apptools.model.adapter import memcache
 from apptools.model.adapter import inmemory
 
 # detect redis support
@@ -52,7 +51,6 @@ class EventEngine(PlatformBridge):
 
         redis = redis.RedisAdapter
         memory = inmemory.InMemoryAdapter
-        memcache = memcache.MemcacheAdapter
 
     redis = datastructures.DictProxy(**{
 
@@ -73,9 +71,9 @@ class EventEngine(PlatformBridge):
             :param *args: Positional arguments to pass to the callable.
             :param **kwargs: Keyword arguments to pass to the callable.
             :returns: A new ``Greenlet``, which is ready to schedule for
-                      coopperative execution. '''
+            coopperative execution. '''
 
-        pass
+        raise NotImplementedError('Method `_spawn` is not yet supported.')
 
     ## === Bindings === ##
     def adapter(self, engine, kind=None, **kwargs):
@@ -85,35 +83,62 @@ class EventEngine(PlatformBridge):
             at :py:attr:`EventEngine.Datastore`.
 
             :param engine: Engine :py:class:`model.adapter.ModelAdapter`
-                           to acquire an adapter to.
+            to acquire an adapter to.
+
+            :param kind: Model ``kind`` name to provision an adapter
+            instance for. ``None`` indicates that a general adapter
+            with default settings should be returned. Defaults to
+            ``None``.
 
             :param **kwargs: Keyword arguments to pass to the newly-
-                             acquired adapter.
+            acquired adapter.
 
             :returns: Newly-prepared/constructed :py:class:`RedisAdapter`,
-                      :py:class:`MemcacheAdapter`, or :py:class:`InMemoryAdapter`,
-                      depending on the value passed in at ``engine``. '''
+            :py:class:`MemcacheAdapter`, or :py:class:`InMemoryAdapter`,
+            depending on the value passed in at ``engine``. '''
 
-        if kind:
-            return engine(**kwargs).channel(kind)
+        if kind: return engine(**kwargs).channel(kind)
         return engine(**kwargs)
 
-    def pipeline(self, engine=Datastore.redis, kind='Realtime'):
+    def pipeline(self, engine=Datastore.redis, kind=None):
 
         ''' Begin execution buffering in the context of an existing
             or newly-created ``Redis`` pipeline.
 
-            :param kind: String ``kind`` name to pass to adapter
-                         methods like :py:meth:`adapter.channel()`.
+            :param engine: Datastore engine to actquire a pipeline
+            for. Choices available at the encapsulated local enum
+            at :py:attr:`EventEngine.Datastore`. Defaults to
+            :py:class:`EventEngine.Datastore.redis`.
 
-            :returns: :py:class:`redis.client.StrictPipeline`. '''
+            :param kind: String ``kind`` name to pass to adapter
+            methods like :py:meth:`adapter.channel`. Defaults to
+            ``None``.
+
+            :returns: :py:class:`redis.client.StrictPipeline`, in
+            the case that ``engine`` is ``Datastore.redis`` and
+            pipelining is supported. Otherwise returns ``None``. '''
 
         return self.adapter(engine, kind).pipeline()
 
     ## === Public Methods === ##
-    def persist(self, entity, pipeline=False):
+    def persist(self, entity, pipeline=None):
 
-        ''' Persist an event. '''
+        ''' Persist an event using underlying data storage
+            mechanisms. Tries as hard as it can to use
+            pipeline-enabled Redis, but falls back to
+            :py:class:`InMemoryAdapter` in the case that it
+            is not supported.
+
+            :param entity: Entity in question that needs to
+            be persisted in storage.
+
+            :param pipeline: Existing pipeline-like object
+            to use in place of a newly constructed one.
+            Defaults to ``None``.
+
+            :returns: Persisted :py:class:`model.Key` for
+            the newly-written entity, or ``pipeline`` if
+            a valid pipeline was passed in. '''
 
         # if pipelining is enabled and supported, start a new one...
         if pipeline and _REDIS and (entity.key.id is not None):
@@ -142,10 +167,11 @@ class EventEngine(PlatformBridge):
             :keyword execute: Boolean flag indicating whether
             :py:meth:`publish` should flush the internal
             pipeline after building, or return it for further
-            operation.
+            operation. Defaults to ``True``.
 
             :keyword pipeline: Replacement, existing pipeline
-            object to use instead of a new one.
+            object to use instead of a new one. Defaults to
+            ``None``.
 
             :returns: The result of the low-level publish
             operation. '''
@@ -179,39 +205,84 @@ class EventEngine(PlatformBridge):
                 channel = channels
                 return self.adapter(self.Datastore.redis, 'Realtime').publish(channel, encoded)
         else:
-            self.logging.info('PubSub disabled, but would publish to channels "%s" with content "%s".' % (channels, value))
+            message = 'PubSub disabled, but would publish to channels "%s" with content "%s".' % (channels, value)
+            self.logging.info(message)
             return
 
-    def subscribe(self, channel, pattern=False):
+    def subscribe(self, channel, pattern=False, _start=False):
 
         ''' Low-level method to subscribe to one or
             multiple ``Redis`` pub/sub streams.
 
-            :keyword _start: Schedule the ``Greenlet`` for
-            immediate execution before returning.
+            :param channel: Target channel to subscribe to,
+            or pattern of channels to subscribe to.
+
+            :param pattern: Flag (``bool``) indicating whether the
+            ``channel`` value is a pattern or literal channel name.
+            Defaults to ``False``.
+
+            :param _start: Schedule the ``Greenlet`` for
+            immediate execution before returning. Defaults to
+            ``False``.
 
             :returns: An optionally-started ``Greenlet``,
             which block-listens for new subscribed
             messages. '''
 
-        pass
+        raise NotImplementedError('Method `subscribe` is not yet supported.')
 
     def increment(self, bucket, delta=1, pipeline=None, force_toplevel=False):
 
         ''' Increment the value of a counter bucket (located
-            at ``bucket``) by the value at ``delta``. '''
+            at ``bucket``) by the value at ``delta``.
+
+            :param bucket: Name (``str``) of the counter
+            bucket key to increment.
+
+            :param delta: Value (``int``, ``float`` or ``long``)
+            to adjust the running total of the target counter by.
+            For example, a delta of ``1`` (the default value) will
+            add ``1`` to the value at ``bucket``, creating it (and
+            conveniently defaulting it to ``0``) the proper bucket
+            if it doesn't exist.
+
+            :param pipeline: Existing pipeline buffer to use for
+            writes that take place in this method. Defaults to
+            ``None``.
+
+            :param force_toplevel: Flag (``bool``) indicating that
+            we wish to force all storage operations to occur in the
+            top-level datastore namespace (disables hash bucketing).
+
+            :raises TypeError: In the case of an invalid type
+            for ``bucket``, which only accepts ``basestring``.
+
+            :raises TypeError: In the case of an invalid type
+            for ``delta``, which only accepts ``float``, ``int``
+            and ``long``.
+
+            :raises ValueError: In the case that ``delta`` is
+            less than 1.
+
+            :returns: Result of the low-level datastore increment
+            operation, or ``pipeline`` if a valid pipeline was
+            presented. '''
 
         from protocol.special import Separators
 
         # check delta type
         if not isinstance(delta, (float, int)):
-            raise ValueError('`delta` value for counter increment operation on '
-                             'bucket "%s" must be an integer or float, got %s.' % (bucket, type(delta)))
+            raise TypeError('`delta` value for counter increment operation on '
+                            'bucket "%s" must be an integer or float, got %s.' % (bucket, type(delta)))
+
+        if delta < 0:
+            raise ValueError('`delta` value for counter increment must be '
+                             'greater than 1. Got: "%s".' % delta)
 
         # check bucket type
         if not isinstance(bucket, basestring):
-            raise ValueError('`bucket` key for counter increment operation must '
-                             'be a string. Received: "%s" of type "%s".' % (bucket, type(bucket)))
+            raise TypeError('`bucket` key for counter increment operation must '
+                            'be a string. Received: "%s" of type "%s".' % (bucket, type(bucket)))
 
         if _REDIS:
 
@@ -243,9 +314,26 @@ class EventEngine(PlatformBridge):
             # execute operation, optionally against a pipeline
             return self.Datastore.redis.execute(handler, None, *opargs, target=pipeline)
 
-    def index_add(self, bucket, key, pipeline=None):
+    def index_add(self, bucket, value, pipeline=None):
 
-        ''' Add the key at ``key`` to the container index
-            specified at ``bucket``. '''
+        ''' Add the value at ``value`` to the container index
+            specified at ``bucket``.
+
+            :param bucket: Bucket identifier (``str``) that specifies
+            the index we wish to write to.
+
+            :param value: Value to add to the index. How the value
+            is written and what structure is used depends on the
+            adapter selected for the write.
+
+            :param pipeline: Existing low-level pipeline to execute
+            the write(s) against, in place of the mainline adapter.
+            Defaults to ``None``.
+
+            :raises NotImplementedError: Always, as this method is
+            currently stubbed.
+
+            :returns: Result of the low-level write operation, or
+            ``pipeline`` if a valid pipeline was presented. '''
 
         raise NotImplementedError('`EventTracker` does not yet support compound key indexes.')
